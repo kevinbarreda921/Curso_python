16782ac8863972782665e868c31b1e92cb0f1082
kevin <kbarreda.waytech@gmail.com>
2026-01-13 11:42:02 -0500

    renombrar carpetas y terminar calculadora

--- Clase 3 - Inicio python/calculadores.py
+++
@@ -1,4 +0,0 @@
-print("=========calculadora==========")
-
-nombre= input("Ingrese  primer numero :")
-edad = input("Ingrese segundo numero:")
---
+++ Clase_3_Inicio_python/calculadores.py
@@ -0,0 +1,9 @@
+print("=========calculadora==========")
+
+Primer_numero = int(input("Ingrese  primer numero :"))
+Segundo_numero = int(input("Ingrese segundo numero:"))
+
+Total=Primer_numero+Segundo_numero
+
+print("Total:")
+print(Total)
